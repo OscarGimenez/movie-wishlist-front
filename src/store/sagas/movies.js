9c85f19a4f484e4ad856898806094dc511d2798f
Giménez Aldabas <o.gimenez.aldabas@accenture.com>
2019-01-10 16:38:46 +0100

    API's text in home

--- src/store/sagas/movies.js
+++ src/store/sagas/movies.js
@@ -8,7 +8,7 @@ import * as actions from '../actions/index';
 export function* setHomeMoviesSaga(action) {
     try {
         const response = yield axios
-            .get('localhost:3000/movie-wishlist/movies/1.0/list/all');
+            .get('https://movie-wishlist-backend.appspot.com/movie-wishlist/movies/1.0/list/all');
         yield put(actions.setHomeMovies(response.data));
     } catch (err) {
         // TODO: Handle error
